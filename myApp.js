e7b127cfd69987fe4efb733921a1d136858e806f
nugliar <hashan.hall@gmail.com>
2021-08-19 15:23:37 -0500

    minor changes

--- myApp.js
+++ myApp.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  process.env.MONGO_URI
 )
 
 let Person;
