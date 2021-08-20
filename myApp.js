bd961a010465eb632eed4ccbbaffb9433d0b5e82
nugliar <hashan.hall@gmail.com>
2021-08-20 11:05:06 -0500

    minor changes

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose')
+
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [{type: String}]
+})
+
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
