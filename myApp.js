36aa8a6daf3bcc1b186b9f5dc0436442c04b8d12
nugliar <hashan.hall@gmail.com>
2021-08-20 22:21:02 -0500

    minor changes

--- myApp.js
+++ myApp.js
@@ -33,6 +33,12 @@ const createAndSavePerson = (done) => {
   })
 };
 
+let arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
+
 const createManyPeople = (arrayOfPeople, done) => {
   Person.create(arrayOfPeople, function(err, data) {
     if (err) return console.error(err)
